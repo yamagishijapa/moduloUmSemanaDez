f3fde3556b7fbde990677a7a39cd02b9239f5221
Yamagishi FMT <thiago_yamagishi@hotmail.com>
2024-03-31 20:51:35 -0300

    Initial commit

---
+++ src/main/java/br/com/fullstack/moduloumsemanadez/ModuloUmSemanaDezApplication.java
@@ -0,0 +1,13 @@
+package br.com.fullstack.moduloumsemanadez;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuloUmSemanaDezApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModuloUmSemanaDezApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fullstack/moduloumsemanadez/ModuloUmSemanaDezApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fullstack.moduloumsemanadez;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModuloUmSemanaDezApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
