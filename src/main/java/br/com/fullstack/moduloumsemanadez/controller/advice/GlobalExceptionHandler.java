a4acad3233ded44fc8b3d20f0054ed095b4efdee
Yamagishi FMT <thiago_yamagishi@hotmail.com>
2024-03-31 23:57:58 -0300

    [M1S10] Ex 2 - CRUD Alunos  - Adicionados tratamentos de erros por um RestControllerAdvice.

---
+++ src/main/java/br/com/fullstack/moduloumsemanadez/controller/advice/GlobalExceptionHandler.java
@@ -0,0 +1,43 @@
+package br.com.fullstack.moduloumsemanadez.controller.advice;
+
+import br.com.fullstack.moduloumsemanadez.response.errorValidation.ValidationErrorDetails;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.multipart.support.MissingServletRequestPartException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<List<ValidationErrorDetails>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        List<ValidationErrorDetails> errors = new ArrayList<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.add(new ValidationErrorDetails(fieldName, errorMessage));
+        });
+        return ResponseEntity.badRequest().body(errors);
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<List<ValidationErrorDetails>> handleHttpMessageNotReadableException(HttpMessageNotReadableException ex) {
+        List<ValidationErrorDetails> errors = new ArrayList<>();
+
+        String campo = "Mensagem HTTP não legível";
+        String errorMessage = "";
+        String message = ex.getMessage();
+        if (message != null && message.contains("Required request body is missing")) {
+            errorMessage = "Corpo da solicitação necessário está faltando";
+        }
+        errors.add(new ValidationErrorDetails(campo, errorMessage));
+        return ResponseEntity.badRequest().body(errors);
+    }
+
+
+}
